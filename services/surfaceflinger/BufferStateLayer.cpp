b1708ae61e38aef90eadceff24a2d2d6f26d9cd9
Ouroboros420 <42john42smith@googlemail.com>
2023-04-24 20:29:08 +0200

    [DNM] revert alleged rice

--- services/surfaceflinger/BufferStateLayer.cpp
+++ services/surfaceflinger/BufferStateLayer.cpp
@@ -459,7 +459,6 @@ bool BufferStateLayer::setHdrMetadata(const HdrMetadata& hdrMetadata) {
 }
 
 bool BufferStateLayer::setSurfaceDamageRegion(const Region& surfaceDamage) {
-    if (mDrawingState.surfaceDamageRegion.hasSameRects(surfaceDamage)) return false;
     mDrawingState.surfaceDamageRegion = surfaceDamage;
     mDrawingState.modified = true;
     setTransactionFlags(eTransactionNeeded);
